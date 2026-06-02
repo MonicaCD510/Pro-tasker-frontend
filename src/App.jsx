016d76b91b7c12276a2d65b4a4027eeddf4b89e6
Monica Davila <monica.davila510@gmail.com>
2026-06-02 06:10:49 -0500

    Create React frontend login page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>Pro-Tasker</h1>
+      <h2>Login</h2>
+
+      <form>
+        <input type="email" placeholder="Email" />
+        <br />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
